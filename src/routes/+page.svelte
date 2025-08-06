64555028f409f135c9849c498a0d44307a6b361e
Matt Gleich <git@mattglei.ch>
2025-08-06 19:06:54 -0400

    feat: initial questions and output text
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,81 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Card, DynamicHead } from '@gleich/ui';
+	import Input from './input.svelte';
+
+	let restaurantName: string | undefined = $state();
+	let subtotal: number | undefined = $state();
+	let tax: number | undefined = $state();
+	let tip: number | undefined = $state();
+</script>
+
+<DynamicHead title="Bill Splitter" description="Simple bill splitter" />
+
+<h1>Bill Splitter</h1>
+
+<div class="sections">
+	<Card>
+		<h3>Initial Questions</h3>
+		<div class="inputs">
+			<Input
+				name="Restaurant Name"
+				placeholder="Daniel's Pizza"
+				type="text"
+				bind:value={restaurantName}
+			/>
+			<Input name="Subtotal" type="number" bind:value={subtotal} />
+			<Input name="Tax" type="number" bind:value={tax} />
+			<Input name="Tip" type="number" bind:value={tip} />
+		</div>
+	</Card>
+
+	<Card>
+		<h3>Output Text</h3>
+		<p class="output-text">
+			Alright everyone here is the breakdown for {restaurantName
+				? restaurantName
+				: '{restaurant name}'}!
+			<br />
+			<br />The general formula is this: (what u got) + (share of tax) + (share of tip) = total you
+			owe me.
+
+			<br />
+			<br />
+			The original bill came out to the following numbers: <br />
+			Subtotal: ${subtotal ?? 0} <br />
+			Tax: ${tax ?? 0} <br />
+			Tip: ${tip ?? 0} <br />
+			Cumulative total: ${(subtotal ?? 0) + (tax ?? 0) + (tip ?? 0)}<br />
+			<br />
+			Here is the breakdown:
+		</p>
+	</Card>
+</div>
+
+<style>
+	h3 {
+		margin-bottom: 15px;
+	}
+
+	.sections {
+		display: flex;
+		flex-direction: column;
+		gap: 30px;
+		margin-top: 30px;
+	}
+
+	.output-text {
+		margin-top: 10px;
+		font-family: 'IBM Plex Mono';
+		font-size: 15px;
+	}
+
+	.inputs {
+		display: flex;
+	}
+
+	.inputs {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		gap: 10px;
+	}
+</style>
---
+++ src/routes/input.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	let {
+		name,
+		placeholder,
+		type,
+		value = $bindable()
+	}: {
+		name: string;
+		placeholder?: string | undefined;
+		type: string;
+		value: string | number | unknown;
+	} = $props();
+
+	const currency = type === 'number';
+</script>
+
+<div class="container">
+	<input
+		class={currency ? 'currency' : ''}
+		{name}
+		{type}
+		placeholder={currency ? '0' : placeholder}
+		bind:value
+	/>
+	<p>{name}</p>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
+
+	input {
+		border: 1px solid var(--border);
+		padding: 10px;
+		text-align: center;
+		background-color: var(--background);
+		font-family: 'IBM Plex Mono';
+		margin: 5px;
+		outline: none;
+		border-radius: 3px;
+		font-size: 16px;
+		width: 100%;
+	}
+
+	.currency {
+		position: relative;
+	}
+
+	.currency::before {
+		content: '$';
+		position: absolute;
+		left: 10px;
+	}
+
+	input[type='number'] {
+		appearance: textfield;
+		-moz-appearance: textfield;
+	}
+	input[type='number']::-webkit-inner-spin-button,
+	input[type='number']::-webkit-outer-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+
+	input:focus {
+		border: 1px solid var(--blue-foreground);
+	}
+
+	.container p {
+		text-align: center;
+		font-size: 14px;
+	}
+</style>
