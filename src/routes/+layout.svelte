c7eb07e1bf9d32152ceeb7fc4ba076fe75840b83
Matt Gleich <git@mattglei.ch>
2025-08-06 17:59:35 -0400

    feat: fonts
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import '@gleich/ui/styles.css';
+	import '../styles/fonts.css';
+	import '../styles/global.css';
 
-	let { children } = $props();
+	import type { Snippet } from 'svelte';
+	import { Layout, siteName } from '@gleich/ui';
+
+	siteName.set('split.mattglei.ch');
+
+	const { children }: { children: Snippet } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
-{@render children?.()}
+<Layout repo="gleich/ui">
+	{@render children()}
+</Layout>
